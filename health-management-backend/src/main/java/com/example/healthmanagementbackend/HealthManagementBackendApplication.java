3432c74c72fd70117a1a1c622d8ae224ca2f52d9
Radu Sabina <sabina.radu02@gmail.com>
2026-04-09 14:45:10 +0300

    init backend

---
+++ health-management-backend/src/main/java/com/example/healthmanagementbackend/HealthManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthManagementBackendApplication.class, args);
+	}
+
+}
---
+++ health-management-backend/src/test/java/com/example/healthmanagementbackend/HealthManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
