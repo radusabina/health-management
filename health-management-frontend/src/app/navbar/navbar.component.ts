afa0cef87349311cff91e1c1f98fa965570b6638
Radu Sabina <sabina.radu02@gmail.com>
2026-04-25 13:26:03 +0300

    navbar component update and focus

--- health-management-frontend/src/app/navbar/navbar.component.ts
+++ health-management-frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -14,28 +14,8 @@ export class NavbarComponent {
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  get isDashboardPage(): boolean {
-    return this.router.url.startsWith('/dashboard');
-  }
-
-  get isOverviewPage(): boolean {
-    return this.router.url.startsWith('/overview');
-  }
-
   logout(): void {
     //this.authService.logout();
     this.router.navigate(['/login']);
-  }
-
-  navigateToDashboard(): void {
-    this.router.navigate(['/dashboard']);
-  }
-
-  navigateToOverview(): void {
-    this.router.navigate(['/overview']);
-  }
-
-  navigateToProfile(): void {
-    this.router.navigate(['/profile']);
   }
 }
